cc23a0c6fc16c4973cd24140fab0bc34fbcc0f21
anas662 <anasishaq@msbitsolutions.com>
2026-08-24 23:56:50 +0500

    changes

--- src/api/projectsApi.js
+++ src/api/projectsApi.js
@@ -1,4 +1,6 @@
-const API_BASE = (process.env.REACT_APP_API_URL || 'http://localhost:8000').replace(/\/$/, '');
+const API_BASE = (
+  process.env.REACT_APP_API_URL || 'https://ae3partnersadmin.testdemolink.com'
+).replace(/\/$/, '');
 
 async function apiGet(path) {
   const res = await fetch(`${API_BASE}${path}`, {
